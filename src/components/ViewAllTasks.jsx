fb940ae76dd3af4d40bb54509aa877bcdfc7556c
EmPi9 <empi9@a25.ru>
2026-01-29 17:23:12 +0300

    feat: Добавил кнопки для удаление и редактирования

--- src/components/ViewAllTasks.jsx
+++ src/components/ViewAllTasks.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import iconEdit from "../assets/icons/fi-rr-edit.svg"
+import iconTrash from "../assets/icons/fi-rs-trash.svg"
 
 export default function ViewAllTasks() {
     const [dataTasks, setDataTasks] = useState([]);
@@ -17,7 +19,12 @@ export default function ViewAllTasks() {
     return <>
         <div className="container">
            {dataTasks.map(item => (
-            <div key={item.id} className="task">{item.title}</div>
+            <div key={item.id} className="task">{item.title} 
+                <div className="task-butts">
+                    <button className="icon"><img src={iconEdit} alt="edit" /> </button>
+                    <button className="icon"><img src={iconTrash} alt="trash" /></button>
+                </div> 
+            </div>
            ))}
         </div>
     </>
